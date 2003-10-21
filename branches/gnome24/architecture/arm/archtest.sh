039594c369da6cdb141030512be467ef49085e27
rene <rene@c5f82cb5-29bc-0310-9cd0-bff59a50e3bc>
2003-10-21 15:54:39 +0000

    remove the merge / changeset generation branch

--- branches/gnome24/architecture/arm/archtest.sh
+++
@@ -1,9 +0,0 @@
-
-case "$ROCKCFG_ARM_ENDIANESS" in
-    EL)
-    	arch_bigendian=no
-	arch_target="arm-unknown-linux-gnu" ;;
-    EB)
-    	arch_bigendian=yes
-	arch_target="arm-unknown-linux-gnu" ;;
-esac
--- branches/gnome24/architecture/arm/kernel.conf.m4
+++
@@ -1,8 +0,0 @@
-define(`MIPS', `MIPS')dnl
-
-# TO BE DONE
-
-include(`kernel-common.conf')
-include(`kernel-scsi.conf')
-include(`kernel-net.conf')
-include(`kernel-fs.conf')
