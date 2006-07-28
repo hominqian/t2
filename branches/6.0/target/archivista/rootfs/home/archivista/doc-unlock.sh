a5a6fcc9c83bcdf71e476ad66d56be449b55b06d
rene <rene@c5f82cb5-29bc-0310-9cd0-bff59a50e3bc>
2006-07-28 11:56:22 +0000

    * copied the 2.2 branch to 6.0

---
+++ branches/6.0/target/archivista/rootfs/home/archivista/doc-unlock.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$UID" -ne 0 ]; then
+        exec gnomesu -t "Document unlocking" \
+        -m "Please enter the system password (root user)^\
+in order to unlock documents." -c $0 -p
+fi
+
+# PATH and co
+. /etc/profile
+
+until [ "$db" ]; do
+        db=`Xdialog --stdout --inputbox "Database to unlock documents:" \
+	        10 40 "$db"` || exit
+	db="${db// /}"
+done
+
+
+until [ "$trange" ]; do
+        trange=`Xdialog --stdout --inputbox "Document range to unlock:
+(e.g. 1-3 or 4)" \
+	        10 40 "$trange"` || exit
+	# remove spaces ...
+	trange="${trange// /}"
+	# remove invalid content
+	if echo "$trange" | grep -q "^\([0-9]\+-[0-9]\+\|[0-9]\+\)$" ; then
+                range="$trange"
+        else
+                Xdialog --infobox "Range not valid!" 8 28
+        fi
+done
+
+unlock="localhost $db root $PASSWD unlock $range"
+/home/cvs/archivista/jobs/avdbutility.pl $unlock 
